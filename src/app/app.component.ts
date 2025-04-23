35e4935934fb4203efc92aa06743d266bf7901b0
Ayubjonzoda Muhammad <m.ayubjonzoda@gmail.com>
2025-04-23 17:24:39 +0500

    item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tileExpert';
+  items = [
+    { name: 'Ссылки', icon: 'link', route: '#' },
+    { name: 'Контакты', icon: 'group', route: '#' },
+    { name: 'Теги', icon: 'numbers', route: '#' },
+    { name: 'Избранное', icon: 'stars_2', route: '#' },
+    { name: 'Посещения', icon: 'history', route: '#' },
+  ];
 }
